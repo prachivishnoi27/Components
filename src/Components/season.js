e8eff17f1d86639311b7adddf90c3546a5197ff5
prachivishnoi <prachi27vishnoi@gmail.com>
2020-11-06 17:46:02 +0530

    seasondisplay

--- src/Components/season.js
+++ src/Components/season.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class Season extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = { lat: null };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null , errorMessage: ''};
+    // }
+
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => console.log(err)
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
+
+    renderContent (){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+
+        if(this.state.lat && !this.state.errorMessage){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner />
+    }
+
     render() {   
         return (
-        <div>Latitude: {this.state.lat}</div>
+            <div className="border red">
+                {this.renderContent()}
+            </div>
         );
     }
 }
