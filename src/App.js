4362fb801ee15b0f1ed22abd9479eeb22a9b1082
prachivishnoi <prachi27vishnoi@gmail.com>
2020-11-08 15:07:46 +0530

    pic added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {BrowserRouter , Route , Switch} from "react-router-dom";
-import Comment from './Components/Comment';
+import Comment from './Components/Comments/Comment';
 import Home from './Components/Home';
-import Season from './Components/season';
+import Season from './Components/Season/season';
+import Pics from './Components/Pics/Pic';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
         <div>
             <Switch>
                 <Route path="/" component={Home} exact />
+                <Route path="/pic" component={Pics} />
                 <Route path="/season" component={Season} />
                 <Route path="/comment" component={Comment} />
             </Switch>
