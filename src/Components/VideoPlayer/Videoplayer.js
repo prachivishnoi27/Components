c7c41f81448fa991c8bbe85e5e61fd87d6c00192
prachivishnoi <prachi27vishnoi@gmail.com>
2020-11-20 16:30:34 +0530

    completed videoplayer

--- src/Components/VideoPlayer/Videoplayer.js
+++ src/Components/VideoPlayer/Videoplayer.js
@@ -2,9 +2,14 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../../api/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class VideoPlayer extends React.Component {
-    state = { videos: []};
+    state = { videos: [], selectedVideo: null};
+
+    componentDidMount(){
+        this.onTermSubmit('dogs');
+    }
 
     onTermSubmit = async term => {
         // console.log(term);
@@ -14,14 +19,31 @@ class VideoPlayer extends React.Component {
             }
         });
 
-        this.setState({videos:response.data.items});
+        this.setState({
+            videos:response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
+
+    onVideoSelect = video => {
+        // console.log(video);
+        this.setState({selectedVideo: video});
+    }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit}/>
-                <VideoList videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
