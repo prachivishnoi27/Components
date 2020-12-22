096c22514d0b8497487e92c46208beab31704012
prachivishnoi <prachi27vishnoi@gmail.com>
2020-12-22 22:42:27 +0530

    songs completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter , Route , Switch} from "react-router-dom";
-import Comment from './Components/Comments/Comment';
+// import Comment from './Components/Comments/Comment';
 import Home from './Components/Home';
 import Season from './Components/Season/season';
 import Pics from './Components/Pics/Pic';
@@ -15,7 +15,7 @@ function App() {
                 <Route path="/" component={Home} exact />
                 <Route path="/pic" component={Pics} />
                 <Route path="/season" component={Season} />
-                <Route path="/comment" component={Comment} />
+                {/* <Route path="/comment" component={Comment} /> */}
                 <Route path="/videoplayer" component={Videoplayer} />
                 <Route path="/songs" component={Song} />
             </Switch>
